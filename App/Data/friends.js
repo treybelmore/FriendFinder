7aa55a2f9e93e5663eaaf1060c1967d3dc565f4b
Trey <treybelmore@gmail.com>
2018-03-12 21:49:18 -0500

    added files

---
+++ App/Data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Snoop Dogg',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/2/2e/Snoop_Dogg_2012.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Leonardo DiCaprio',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Leonardo_DiCaprio.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jennifer Lawrence',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/d/d8/Jennifer_Lawrence.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Tom Brady',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Tom_Brady_2017.JPG',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'OJ Simpson',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/b/b3/Mug_shot_of_O.J._Simpson.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Martha Stewart',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Martha_Stewart.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Tony Robbins',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/5/5e/Tony_Robbins.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Gary Vaynerchuk',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Gary_Vaynerchuk_public_domain.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Mark Zuckerberg',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Mark_Zuckerberg_CEO_Facebook.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# FriendFinder
+Friend Finder Application
+
+Description
+
+Friend Finder implements friend matching for celebrities based on the user's responses to a ten question survey. The user responds to questions with values from 1 (Strongly Disagree) to 5 (Strongly Agree). When the survey is submitted, an existing user record closest to the current user's responses is found and returned. The closest set of user responses is defined as the set with the lowest absolute difference for all ten questions combined.
+
+Friend Finder application is meant to simulate a simple dating app. The application is implemented using a Node.js and Express server on the back end and the Materialize CSS framework on the front end.
+
+
+Installation
+To install the application follow the instructions below:
+
+git clone 
+cd friend-finder
+npm install
+Running Locally
+
+
+node server.js
+The application will now be running locally on PORT, in this case that is port 3000. You can then access it locally from your browser at the URL localhost:PORT, in this case localhost:3000.
